761fd11e6a151e75b1ff39e0b775c783ee9c4617
Shreshtha <tasnuva369@gmail.com>
2022-09-25 08:20:46 +0600

    fourth commits

---
+++ JS/bookList.js
@@ -0,0 +1,48 @@
+const bookList = [
+  {
+    name: "The India Story",
+    author: "Bimal Jalal",
+  },
+  {
+    name: "Listen to Your Heart: The London Adventure",
+    author: "Ruskin Bond",
+  },
+  {
+    name: "Business of Sports: The Winning Formula for Success",
+    author: "Vinit Karnik",
+  },
+  {
+    name: "A Place Called Home",
+    author: "Preeti Shenoy",
+  },
+  {
+    name: "Modi @20: Dreams Meeting Delivery",
+    author: "VP Venkaiah Naidu",
+  },
+  {
+    name: "The Struggle for Police Reforms in India",
+    author: "Smriti Irani",
+  },
+  {
+    name: "Lal Salam",
+    author: "Former CAG Vinod Rai",
+  },
+  {
+    name: "Amit Shah Ani Bhajapachi Vatchal",
+    author: "Rajesh Talwar",
+  },
+  {
+    name: "Monsoon",
+    author: "Sahitya Akademi",
+  },
+  {
+    name: "Mamata: Beyond 2021",
+    author: "Jayanta Ghosal",
+  },
+];
+
+// for (let i = 0; i < bookList.length; i++) {
+//   let li = document.createElement("li");
+//   li.style.fontSize = 20;
+// }
+console.log(bookList);
